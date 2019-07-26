b6f13ab1a4c55f8273dcfa771aa8be79d8f154fc
srisrinu <srinivasroy2018@gmail.com>
2019-07-26 10:00:53 +0530

    commit

--- Practice/src/ImpProblems/NthPrime.java
+++ Practice/src/ImpProblems/NthPrime.java
@@ -3,26 +3,17 @@ package ImpProblems;
 import java.util.Scanner; 
 public class NthPrime {
 public static void main(String[] args) {
-
 Scanner sc = new Scanner(System.in);
-
 System.out.print("Enter n to compute the nth prime number: ");
-
 int nth = sc.nextInt();
-
 int num, count, i;
 num=1;
 count=0;
-
 while (count < nth){
 num=num+1;
 for (i = 2; i <= num; i++){
 if (num % i == 0) {
-break;
-}
-}
+break;}}
 if ( i == num){
-count = count+1;
-}
-}
+count = count+1;}}
 System.out.println("Value of nth prime: " + num); } }
