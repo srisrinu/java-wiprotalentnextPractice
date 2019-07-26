ad46acb8a10e62a267f33ce856e1d33577a59f26
srisrinu <srinivasroy2018@gmail.com>
2019-07-26 09:59:11 +0530

    commit

--- Practice/src/ImpProblems/EncodedArray.java
+++ Practice/src/ImpProblems/EncodedArray.java
@@ -6,8 +6,7 @@ class EncodedArray {
 		int[] out = new int[input1.length];
 		out[out.length - 1] = input1[input1.length - 1];
 		for (int i = input1.length - 1; i > 0; i--) {
-			out[i - 1] = input1[i - 1] - out[i];
-		}
+			out[i - 1] = input1[i - 1] - out[i];}
 		int sum = 0;
 		for (int item : out)
 			sum += item;
